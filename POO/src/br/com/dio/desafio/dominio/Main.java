e3c68e9b02290077d012a656cd538abd30b074da
Kauã dos Santos Silva <kauadossantossilva02@gmail.com>
2024-09-09 11:57:33 -0300

    Endendendo POO

---
+++ POO/src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,54 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Curso curso1 = new Curso();
+		curso1.setTitulo("curso java");
+		curso1.setDescricao("descrição curso java");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("curso python");
+		curso2.setDescricao("descrição curso python");
+		curso2.setCargaHoraria(4);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("mentoria java");
+		mentoria.setDescricao("descricao mentoria java");
+		mentoria.setData(LocalDate.now());
+		/*
+		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria);
+		*/
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descricao Bootcamp");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev devKaua = new Dev();
+		devKaua.setNome("Kauã");
+		devKaua.inscreverBootcamp(bootcamp);
+		
+		Dev devCarlos = new Dev();
+		devCarlos.setNome("Carlos");
+		devCarlos.inscreverBootcamp(bootcamp);
+		
+		System.out.println("Conteudos Inscristos Kauã" + devKaua.getConteudosInscritos());
+		devKaua.progredir();
+		System.out.println("Conteudos Inscristos Kauã" + devKaua.getConteudosInscritos());
+		System.out.println("Conteudos Concluidos Kauã" + devKaua.getConteudosConcluidos());
+		System.out.println("--------------------------------");
+		
+		System.out.println("Conteudos Inscristos Carlos" + devCarlos.getConteudosInscritos());
+		devCarlos.progredir();
+		System.out.println("Conteudos Inscristos Carlos" + devCarlos.getConteudosInscritos());
+		System.out.println("Conteudos Concluidos Carlos" + devCarlos.getConteudosConcluidos());
+		System.out.println("--------------------------------");
+	}
+}
